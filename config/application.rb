0255b965abd2ff3fdfa7c11e4d4f7860677e36c2
isaacmeedinaa <isaac.meedinaa@gmail.com>
2020-05-27 17:17:22 -0500

    edit for coupons complete

--- config/application.rb
+++ config/application.rb
@@ -37,7 +37,7 @@ module CoolCouponsApi
     config.middleware.insert_before 0, Rack::Cors do
       allow do
           origins '*'
-          resource '*', headers: :any, methods: [:get, :post, :delete]
+          resource '*', headers: :any, methods: [:get, :post, :patch, :delete]
       end
     end
   end
